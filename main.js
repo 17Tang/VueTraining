9f8f04157035ef869cde4a0c230c35b95c2dd27b
17Tang <yabitang0417@gmail.com>
2020-10-01 09:10:36 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+    el: '#app',
+    data:{
+        product: 'Socks',
+        image: './vmSocks-blue.jpg',
+        inStock: true,
+        details: ["80% cotton","20% polyester","Gender neutral"]
+    }
+})
